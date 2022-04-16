255d4c0610ee303746ebcd6650008469c842a162
Ho3ein Zarrabi <ho3einzarrabi@gmail.com>
2022-04-16 17:40:52 +0430

    Added upload and edit profile functionality

--- src/graphql/resolver/user-resolver.js
+++ src/graphql/resolver/user-resolver.js
@@ -1,53 +1,82 @@
+import User from "../../models/user";
+import Tag from "../../models/tag";
+import { createWriteStream, unlink } from "fs";
+import path from "path";
+import authorizeUser from "../../lib/auth";
+import { finished } from "stream/promises";
 
-
-
-import User from '../../models/user'
-import Tag from '../../models/tag'
-
-import authorizeUser from '../../lib/auth'
-
+path.join(process.cwd(), "/src/db/users");
 
 export default {
-  root: {
-
-  },
+  root: {},
   Query: {
-    me: async (_, data, {user}) => {
+    me: async (_, data, { user }) => {
       try {
+        const thisuser = await authorizeUser(user);
 
-        const thisuser = await authorizeUser(user)
-
-        return thisuser
+        return thisuser;
       } catch (error) {
-        console.log(error)
-        throw error
+        console.log(error);
+        throw error;
       }
-    }
+    },
   },
   Mutation: {
     signup: async (_, data) => {
-      
       try {
-        
-        const token = await User.signup(data)
-        
-        return {
-          token
-        }
+        const token = await User.signup(data);
 
+        return {
+          token,
+        };
       } catch (error) {
-        throw error
+        throw error;
       }
     },
     login: async (_, data) => {
       try {
-        const token = await User.login(data)
+        const token = await User.login(data);
         return {
-          token
-        }
+          token,
+        };
       } catch (error) {
-        throw error
+        throw error;
       }
-    }
-  }
-}
+    },
+    editMe: async (_, { name, img }, { user }) => {
+      console.log("************");
+      console.log(img);
+      console.log(name);
+      try {
+        const thisuser = await authorizeUser(user);
+
+        if (img) {
+          const { createReadStream, mimetype } = await img;
+          const wtf = mimetype.split("/")[1];
+          if (wtf !== "jpeg" && wtf !== "jpg" && wtf !== "png")
+            throw new Error("bad request: Invalid file type");
+          const stream = createReadStream();
+          // await storeImageUpload({ stream, userId: thisuser._id });
+          const out = createWriteStream(
+            path.join(
+              process.cwd(),
+              `/src/db/users/${thisuser._id}/profile.${wtf}`
+            )
+          );
+          stream.pipe(out);
+          await finished(out);
+        }
+
+        console.log("salamasadsad");
+        await User.findByIdAndUpdate(thisuser._id, { name });
+
+        return {
+          status: 200,
+          msg: "ok!",
+        };
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+};
