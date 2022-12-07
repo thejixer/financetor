28eb1532dc0a6c9e8c8ae305306fac83a5299188
naser karami <naser.karami76@gmail.com>
2022-12-07 10:48:09 +0330

    fix a bug where db didnt get created automaticly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-// import '@lib/global'
 import runServer from './server/run-server'
 import buildSchema from './graphql/schema'
 import resolvers from './graphql/resolver'
+import path from 'path'
+import { existsSync, mkdirSync } from 'fs'
+
+const dbDirectory = path.join(process.cwd(), '/src/db')
+const dbuserDirectory = path.join(process.cwd(), '/src/db/users')
+
+if (!existsSync(dbDirectory)) mkdirSync(dbDirectory)
+if (!existsSync(dbuserDirectory)) mkdirSync(dbuserDirectory)
 
 const typeDefs = buildSchema()
 
